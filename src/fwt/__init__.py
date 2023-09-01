c35a4ff22a6f2f5fc3c2d2d500a0512a48bc7a76
Alastair Marchant <alastair.marchant@intuety.ltd>
2023-09-01 18:37:55 +0100

    Add version to __init__.py

--- src/fwt/__init__.py
+++ src/fwt/__init__.py
@@ -1,1 +1,8 @@
 """Python CLI tool for managing assets within Foundry Virtual Tabletop."""
+from importlib.metadata import PackageNotFoundError, version
+
+
+try:
+    __version__ = version("foundry-world-tools")
+except PackageNotFoundError:
+    __version__ = "unknown"
